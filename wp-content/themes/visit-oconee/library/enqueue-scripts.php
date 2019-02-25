41b61dee45fd433f9edc5b4213856ce2c5b92867
Joel Newcomer <joel@drumcreative.com>
2019-02-25 09:47:17 -0500

    tweak

--- wp-content/themes/visit-oconee/library/enqueue-scripts.php
+++ wp-content/themes/visit-oconee/library/enqueue-scripts.php
@@ -9,16 +9,6 @@
  * @since DrumRoll 1.0.0
  */
 
-// Defer jQuery Parsing using the HTML5 defer property
-if (!(is_admin() )) {
-    function defer_parsing_of_js ( $url ) {
-        if ( FALSE === strpos( $url, '.js' ) ) return $url;
-        if ( strpos( $url, 'jquery.js' ) ) return $url;
-        // return "$url' defer ";
-        return "$url' defer onload='";
-    }
-    add_filter( 'clean_url', 'defer_parsing_of_js', 11, 1 );
-} 
 
 if ( ! function_exists( 'drumroll_scripts' ) ) :
 	function drumroll_scripts() {
